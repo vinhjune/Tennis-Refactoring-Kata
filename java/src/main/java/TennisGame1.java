9eeffb7b8758a3e028e96ea8e3954e24608d13a2
VinhJune <hoangthevinh.htv@gmail.com>
2016-06-19 23:03:05 +0200

    changed tennisgame1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -11,11 +11,18 @@ public class TennisGame1 implements TennisGame {
         this.player2Name = player2Name;
     }
 
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        /*hard coded name is wrong
+        one liner condition should also be inside {}
+        conditions must be precise for both player1 and player2*/
+//        if (playerName == "player1")
+        if (playerName == this.player1Name) {
             m_score1 += 1;
-        else
+//        else
+        } else if (playerName == this.player2Name) {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
@@ -43,17 +50,30 @@ public class TennisGame1 implements TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) {
+                score ="Advantage player1";
+            }
+            else if (minusResult ==-1) {
+                score ="Advantage player2";
+            }
+            else if (minusResult>=2) {
+                score = "Win for player1";
+            }
+            else {
+                score ="Win for player2";
+            }
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) {
+                    tempScore = m_score1;
+                }
+                else {
+                    score+="-";
+                    tempScore = m_score2;
+                }
                 switch(tempScore)
                 {
                     case 0:
